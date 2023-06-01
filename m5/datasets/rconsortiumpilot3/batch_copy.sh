10208f3cb2468425f08337f85ea37e4ae6257641
Declan Hodges <declanhodges2@hotmail.com>
2023-06-01 14:44:25 +0000

    8_restructure_directories@main: Clone from main rather than test branch.

--- m5/datasets/rconsortiumpilot3/batch_copy.sh
+++ m5/datasets/rconsortiumpilot3/batch_copy.sh
@@ -2,7 +2,7 @@
 
 cd /cloud/project/m5/datasets/rconsortiumpilot3
 
-git clone https://github.com/RConsortium/submissions-pilot3-adam.git --branch batch_copy_test --single-branch ./submission-pilot3-adam --depth 1
+git clone https://github.com/RConsortium/submissions-pilot3-adam.git --branch main --single-branch ./submission-pilot3-adam --depth 1
 
 mv ./submission-pilot3-adam/submission/sdtm/*.xpt ./tabulations/sdtm
 mv ./submission-pilot3-adam/submission/adam/*.xpt ./analysis/adam
